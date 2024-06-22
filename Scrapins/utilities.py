02eb59db9bacacc28a1447f1252273a5a8548216
Grimmins <lukas.koltes@hotmail.fr>
2024-06-22 11:28:12 +0200

    prettifier utilities.py

--- Scrapins/utilities.py
+++ Scrapins/utilities.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -18,3 +19,18 @@ def save_html(output_file, html):
 def load_html(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
+
+def prettify_html_file(input_file, output_file):
+    # Charger le contenu du fichier HTML
+    with open(input_file, 'r', encoding='utf-8') as file:
+        html_content = file.read()
+
+    # Analyser le contenu HTML avec BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Utiliser prettify pour formater le HTML
+    pretty_html = soup.prettify()
+
+    # Écrire le HTML formaté dans un nouveau fichier
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(pretty_html)
